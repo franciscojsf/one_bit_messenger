87cc5bf6bf66bd974b4097be2fa29cee1e19732e
Francisco Sousa <francisco.filho@ufpi.edu.br>
2020-01-08 18:20:31 -0300

    Aula 2 - Criando as views e controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   #get 'home/index'
   root to: "home#index"
+  resources :contacts, only: %i(index new create destroy)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
